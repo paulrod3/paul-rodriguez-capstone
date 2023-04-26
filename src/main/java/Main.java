00e238a081b5b398282cd5e0a562ead23c982d2a
Paul <paul_rodriguez3@yahoo.com>
2023-04-26 06:18:56 -0500

    added main class and Renovation Expense class to project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<RenovationExpense> expenses = new ArrayList<>();
+        boolean done = false;
+
+        while (!done) {
+            System.out.println("Enter the type of expense (or 'done' to finish):");
+            String type = scanner.nextLine();
+
+            if (type.equalsIgnoreCase("done")) {
+                done = true;
+            } else {
+                System.out.println("Enter the amount spent:");
+                double amount = scanner.nextDouble();
+                scanner.nextLine();
+
+                System.out.println("Enter the date of the expense (in mm/dd/yyyy format):");
+                String date = scanner.nextLine();
+
+                RenovationExpense expense = new RenovationExpense(type, amount, date);
+                expenses.add(expense);
+            }
+        }
+
+        System.out.println("Here are the renovation expenses:");
+        for (RenovationExpense expense : expenses) {
+            System.out.println(expense.getType() + " - $" + expense.getAmount() + " - " + expense.getDate());
+        }
+    }
+
+
 }
---
+++ src/main/java/RenovationExpense.java
@@ -0,0 +1,26 @@
+public class RenovationExpense {
+
+    private String type;
+    private double amount;
+    private String date;
+
+    public RenovationExpense(String type, double amount, String date) {
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+
+}
